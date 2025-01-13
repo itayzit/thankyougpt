e284cbe6f06028114f8d8c3fbe4f8117aa43d57f
itayzit <itayzit@gmail.com>
2025-01-13 11:11:15 +0100

    update gitignore

---
+++ app/api/chat/route.ts
@@ -0,0 +1,41 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai'
+
+
+const eventTypeToPrompt: Record<string, string> = {
+  "Coffee chat": "a coffee chat",
+  "EIS": "an employer information session",
+  "Other": "recruiting events"
+}
+
+const formalityLevels: Record<number, string> = {
+  1: "casual",
+  2: "engaging", 
+  3: "formal yet engaging",
+  4: "formal",
+  5: "very formal"
+}
+
+const systemPromptTemplate = 
+  "This GPT acts as MBA student at a top business school, writing thank-you emails after meeting professionals at {eventType}. Its role is to draft professional and intriguing thank-you emails based on notes provided from networking conversations.\n" +
+  "The GPT should tailor each response to reflect the unique points of the conversation, express gratitude, and reinforce interest in the company or industry. It will always keep a {formality} tone, reflecting both ambition and genuine appreciation. If there's anything in common between the sender and the receiver, mention it.\n" +
+  "All thank-you emails should be short - only {lines} lines. Only If relevant, this GPT should ask a question like \"What do you think I can do at Wharton to be better at X\". It should mention the person's name in the beginning of the email (as in \"My name is [Your Name]\"). This GPT is short and concise, and sounds natural for a native English speaker."
+
+export async function POST(req: Request) {
+  const { messages, lines, formality, eventType } = await req.json()
+  console.log("blablabla")
+  const systemPrompt = systemPromptTemplate
+    .replace("{lines}", lines.toString())
+    .replace("{eventType}", eventTypeToPrompt[eventType])
+    .replace("{formality}", formalityLevels[formality])
+  console.log(systemPrompt)
+  const result = streamText({
+    model: openai('gpt-4'),
+    messages: [
+      { role: 'system', content: systemPrompt },
+      ...messages
+    ]
+  })
+  return result.toDataStreamResponse()
+}
+
